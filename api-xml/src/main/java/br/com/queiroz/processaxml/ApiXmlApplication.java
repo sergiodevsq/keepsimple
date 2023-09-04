481744bf9d8fa943e9360138fd89ddbecd990c7a
Sergio Queiroz <sergio@sq.dev.br>
2023-09-04 08:18:07 -0300

    Commit inicial

---
+++ api-xml/src/main/java/br/com/queiroz/processaxml/ApiXmlApplication.java
@@ -0,0 +1,13 @@
+package br.com.queiroz.processaxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiXmlApplication.class, args);
+	}
+
+}
---
+++ api-xml/src/test/java/br/com/queiroz/processaxml/ApiXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.queiroz.processaxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
